2ee47cf922db7d52caa69fc2b65f5acfa7931780
LuisE <bellyluis@gmail.com>
2019-02-12 11:00:25 -0500

    Initializing Data with Spring Framework Events

--- src/main/java/guru/springframework/spring5webapp/repositories/BookRepository.java
+++ src/main/java/guru/springframework/spring5webapp/repositories/BookRepository.java
@@ -4,5 +4,7 @@ import guru.springframework.spring5webapp.model.Book;
 import org.springframework.data.repository.CrudRepository;
 
 public interface BookRepository extends CrudRepository<Book, Long> {
-    
+
 }
+
+
